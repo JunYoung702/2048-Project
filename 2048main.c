f92e3596a0235a60e3eae03c4a4770fa3820b85f
JunYoung702 <702wnsdud@gmail.com>
2019-12-11 01:23:48 +0900

    hide cursor/input without enter

--- 2048main.c
+++ 2048main.c
@@ -1,9 +1,9 @@
+#include "./lib-cross.h"
+#include "./mainfeatures.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "./mainfeatures.h"
-#include "./lib-cross.h"
 #define SIZE 4
 bool wincounter = false;
 bool status = false;
@@ -15,38 +15,44 @@ typedef struct _2048tiles
 {
     int tiles[SIZE][SIZE];
     struct _2048tiles *next;
-}tile;
+} tile;
 int main(void)
 {
-	char command;
-	// get input
-	for (int i = 0; i < SIZE; ++i)
-		scanf("%d %d %d %d", &numbers[i][0], &numbers[i][1], &numbers[i][2], &numbers[i][3]);
+    hide_cursor();
+    char command;
+    // get input
+    for (int i = 0; i < SIZE; ++i)
+        scanf("%d %d %d %d", &numbers[i][0], &numbers[i][1], &numbers[i][2], &numbers[i][3]);
     for (int i = 0; i < SIZE; ++i)
         for (int j = 0; j < SIZE; ++j)
             if (numbers[i][j] == 0)
                 zeros++;
-    // TODO: win, lose, merge + move_left into one function 
     draw(score);
-    while (scanf("%c", &command) != EOF)
+    while ((command = getch()) != EOF)
     {
-		if (command == ' ' || command == '\n')
+        if (command == ' ' || command == '\n')
             continue;
+        if (command == EOF)
+            break;
         switch (command)
         {
         case 'a':
+            screen_clear();
             score += move_left(numbers);
             break;
 
         case 'w':
+            screen_clear();
             score += move_up(numbers);
             break;
 
         case 's':
+            screen_clear();
             score += move_down(numbers);
             break;
 
         case 'd':
+            screen_clear();
             score += move_right(numbers);
             break;
 
@@ -61,28 +67,32 @@ int main(void)
             printf("You Win!");
             status == true;
         }
-		if (zeros == 0) // Test losing condition
-		{
-			int temp[SIZE][SIZE];
-			for (int i = 0; i < SIZE; ++i)
-				for (int j = 0; j < SIZE; ++j)
-					temp[i][j] = numbers[i][j];
+        if (zeros == 0) // Test losing condition
+        {
+            int temp[SIZE][SIZE];
+            for (int i = 0; i < SIZE; ++i)
+                for (int j = 0; j < SIZE; ++j)
+                    temp[i][j] = numbers[i][j];
             losecounter += move_left(temp);
-			for (int i = 0; i < SIZE; ++i)
-				for (int j = 0; j < SIZE; ++j)
-					temp[i][j] = numbers[i][j];
+            for (int i = 0; i < SIZE; ++i)
+                for (int j = 0; j < SIZE; ++j)
+                    temp[i][j] = numbers[i][j];
             losecounter += move_down(temp);
-			for (int i = 0; i < SIZE; ++i)
-				for (int j = 0; j < SIZE; ++j)
-					temp[i][j] = numbers[i][j];
+            for (int i = 0; i < SIZE; ++i)
+                for (int j = 0; j < SIZE; ++j)
+                    temp[i][j] = numbers[i][j];
             losecounter += move_right(temp);
-			for (int i = 0; i < SIZE; ++i)
-				for (int j = 0; j < SIZE; ++j)
-					temp[i][j] = numbers[i][j];
+            for (int i = 0; i < SIZE; ++i)
+                for (int j = 0; j < SIZE; ++j)
+                    temp[i][j] = numbers[i][j];
             losecounter += move_up(temp);
             if (!losecounter)
+            {
                 printf("You Lose!");
-		}
+                break;
+            }
+        }
     }
+    show_cursor();
     return 0;
 }
--- mainfeatures.c
+++ mainfeatures.c
@@ -1,8 +1,8 @@
+#include "./mainfeatures.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "./mainfeatures.h"
 #define SIZE 4
 extern bool wincounter;
 extern bool status;
@@ -10,45 +10,46 @@ extern int score, zeros;
 extern int numbers[SIZE][SIZE];
 void draw(int score)
 {
-	for (int i = 0; i < SIZE; ++i)
-	{
-		printf("-------------------------\n");
-		printf("|     |     |     |     |\n");
-		printf("| %4d| %4d| %4d| %4d|\n", numbers[i][0], numbers[i][1], numbers[i][2], numbers[i][3]);
-		printf("|     |     |     |     |\n");
-	}
-	printf("-------------------------");
-	printf("\nScore: %d\n", score);
+    for (int i = 0; i < SIZE; ++i)
+    {
+        printf("-------------------------\n");
+        printf("|     |     |     |     |\n");
+        printf("| %4d| %4d| %4d| %4d|\n", numbers[i][0], numbers[i][1], numbers[i][2], numbers[i][3]);
+        printf("|     |     |     |     |\n");
+    }
+    printf("-------------------------");
+    printf("\nScore: %d\n", score);
 }
 void number(void)
 {
-	srand(time(NULL));
-	int x = rand() % SIZE;
-	int y = rand() % SIZE;
-	while (numbers[x][y] != 0)
-	{
-		if (rand() % 2 == 0)
-			x++;
-		else
-			y++;
-	}
-	numbers[x][y] = 2;
+    srand(time(NULL));
+    int x = rand() % SIZE;
+    int y = rand() % SIZE;
+    while (numbers[x][y] != 0)
+    {
+        if (rand() % 2 == 0)
+            x++;
+        else
+            y++;
+    }
+    numbers[x][y] = 2;
+    zeros--;
 }
 void rotate_left(void)
 {
-	int temp[SIZE][SIZE];
-	for (int i = 0; i < SIZE; ++i)
-		for (int j = 0; j < SIZE; ++j)
-			temp[i][j] = numbers[i][j];
-	for (int i = 0; i < SIZE; ++i)
-		for (int j = 0; j < SIZE; ++j)
-			numbers[i][j] = temp[j][SIZE - 1 - i];
+    int temp[SIZE][SIZE];
+    for (int i = 0; i < SIZE; ++i)
+        for (int j = 0; j < SIZE; ++j)
+            temp[i][j] = numbers[i][j];
+    for (int i = 0; i < SIZE; ++i)
+        for (int j = 0; j < SIZE; ++j)
+            numbers[i][j] = temp[j][SIZE - 1 - i];
 }
 int move_left(int arr[SIZE][SIZE])
 {
     int scoreadded = 0;
-/* move without merge code */
-	for (int i = 0; i < SIZE; ++i)
+    /* move without merge code */
+    for (int i = 0; i < SIZE; ++i)
         for (int j = 0; j < SIZE; ++j)
             for (int k = 0; k < j; ++k)
                 if (arr[i][k] == 0)
@@ -57,7 +58,7 @@ int move_left(int arr[SIZE][SIZE])
                     arr[i][k] = arr[i][j];
                     arr[i][j] = temp;
                 }
-/* Merge code */
+    /* Merge code */
     for (int i = 0; i < SIZE; ++i)
         for (int j = 0; j < SIZE - 1; ++j)
             if (arr[i][j] == arr[i][j + 1])
@@ -69,7 +70,7 @@ int move_left(int arr[SIZE][SIZE])
                 if (arr[i][j] == 2048)
                     wincounter = true;
             }
-	for (int i = 0; i < SIZE; ++i)
+    for (int i = 0; i < SIZE; ++i)
         for (int j = 0; j < SIZE; ++j)
             for (int k = 0; k < j; ++k)
                 if (arr[i][k] == 0)
